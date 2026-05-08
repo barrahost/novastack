9282a9a2c9b2d7d3c9eb264ee3d873557f524b12
d-infras <46763217+barrahost@users.noreply.github.com>
2026-05-08 17:26:16 +0000

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
